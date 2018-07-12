0535a3349fc7c961658cf69c79ea8e38a0803ed6
xiaohongma <565672848@qq.com>
2018-07-12 15:39:26 +0800

    4444

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 int main(int argc, char **argv) {
     std::cout << "Hello, Kun!" << std::endl;
     ///fgtyj
+    
+
     //jdethjgh
-    std::cout << "Hello, my git" << std:: endl;
+    std::cout << "Hello, my git"<< std::endl;
     return 0;
 }
