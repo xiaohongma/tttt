5e9f3b34249f10a0bda4bec9173e2c4a8c23c5a8
xiaohongma <565672848@qq.com>
2018-07-12 14:45:18 +0800

    sdnlks

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 int main(int argc, char **argv) {
     std::cout << "Hello, Kun!" << std::endl;
-    
+    ///fgtyj
+    //jdethjgh
+    std::cout << "Hello, my git" << std:: endl;
     return 0;
 }
